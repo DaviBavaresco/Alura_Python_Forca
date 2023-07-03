b1cabe779c2a3543fe73a7d55691c56629c2e8e9
DaviBavaresco <100875783+DaviBavaresco@users.noreply.github.com>
2023-07-03 16:50:42 -0300

    Initial commit

---
+++ forca.py
@@ -0,0 +1,30 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra: ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute==letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando")
+
+
+
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
